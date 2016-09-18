4e0c1d927205a640c0a05ad8ac39f7413379eca3
map25 <mccoypatino@gmail.com>
2016-09-18 07:12:16 -0400

    Add list view of events

---
+++ app/src/main/java/com/example/loganpatino/hackmit_2016/EventList.java
@@ -0,0 +1,13 @@
+package com.example.loganpatino.hackmit_2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+    }
+}
